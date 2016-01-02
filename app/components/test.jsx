801a8f7c1e245b903c0fb685ddfe50f7a1b83407
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-02 23:12:53 +0800

    learning es2015 class

--- app/components/test.jsx
+++ app/components/test.jsx
@@ -7,19 +7,20 @@ import React from 'react'
 // }
 
 
-export default class Para extends React.createClass({
-    getInitialState: function () {
-        return {liked: false}
-    },
-    handleClick: function (event) {
-        this.setState({liked: !this.state.liked})
-    },
-    render: function () {
-        var test = this.state.liked ? 'like' : 'havent\'t liked'
+export default class Para extends React.Component {
+    constructor() {
+        super()
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick(event) {
+        alert('sadasd')
+    }
+
+    render() {
         return (
             <p onClick={this.handleClick}>
-                You {text} this. Click to toggle.
+                You  this. Click to toggle.
             </p>
         )
     }
-})
+}
--- app/main.js
+++ app/main.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 // import Hello from './components/test.jsx'
 import Para from './components/test.jsx'
 
-function main() {
+// function main() {
     ReactDOM.render(<Para />, document.getElementById('app'));
-}
+// }
 
-main()
+// main()
