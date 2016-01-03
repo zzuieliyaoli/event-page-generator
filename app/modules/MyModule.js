0047a1c0cb2ff81c9ef7ae6c46d5abbb872395cc
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-03 22:53:40 +0800

    learning es2015

--- app/modules/MyModule.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-export function component() {
-    var element = document.createElement('h2')
-    element.innerHTML = 'Hello World ds哈哈哈sdasdddasdasdasddasdasd'
-    return element
-}
