abd1d1c20f8e41cfd3778ca53f4d339582804731
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-05 23:04:22 +0800

    了解react

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 // import Hello from './components/test.jsx'
-import Para from './components/test.jsx'
+// import Para from './components/test.jsx'
 
 // function main() {
     ReactDOM.render(<Para />, document.getElementById('app'));
