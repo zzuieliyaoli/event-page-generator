dad054030dd6c79f57ee49be4722ec589dfffab1
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-06 22:57:36 +0800

    学习

--- app/main.js
+++ app/main.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom'
 // import Hello from './components/test.jsx'
 // import Para from './components/test.jsx'
 
+class Para extends React.Component {
+    constructor () {
+        this.super()
+        this.eventHandler = this.eventHandler.bind(this)
+    }
+
+    eventHandler () {
+
+    }
+
+}
+
 // function main() {
     ReactDOM.render(<Para />, document.getElementById('app'));
 // }
