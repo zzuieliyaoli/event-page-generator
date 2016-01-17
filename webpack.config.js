add691c63b271e8ca24db59ee93f85ee5628f69a
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-17 23:36:57 +0800

    bower init and node server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path')
 var node_modules = path.resolve(__dirname, 'node_modules')
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js')
-var pathToReactDom = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js');
+var pathToReactDom = path.resolve(node_modules, 'react-dom/dist/react-dom.min.js')
 
 module.exports = {
-    entry: ['webpack/hot/dev-server', path.resolve(__dirname, 'app/main.js')],
+    entry: [
+        'webpack/hot/dev-server', 
+        path.resolve(__dirname, 'app/main.js')
+    ],
     resolve: {
         alias: {
           'react': pathToReact,
