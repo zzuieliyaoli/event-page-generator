a2c839435706a9b2ac8c54f63597579b2aa28cbc
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-21 23:02:21 +0800

    learing how to write test cases

--- test/components/Navbar-test.js
+++ test/components/Navbar-test.js
@@ -6,3 +6,23 @@ expect.extend(expectJSX)
 
 import Navbar from './../../app/components/Navbar'
 
+describe('Navbar', () => {
+    it('Navbar works', () => {
+        let renderer = createRenderer()
+        renderer.render(<Navbar />)
+        let actualElement = renderer.getRenderOutput()
+        let expectedElement = ( <Router>
+                <Route path="/" component={App}>
+                <IndexRoute component={Dashboard} />
+                  <Route path="about" component={About} />
+                  <Route path="inbox" component={Inbox}>
+                    <Route path="/messages/:id" component={Message} />
+                    <Redirect from="messages/:id" to="/messages/:id" />
+                  </Route>
+                </Route>
+            </Router>)
+        expect(actualElement).toEqualJSX(expectedElement)
+
+
+    })
+})
