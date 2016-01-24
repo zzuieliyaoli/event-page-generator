2fd96636c7ee28bacf426ae7222c33bba2fcded5
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-24 23:05:22 +0800

    edit test

--- test/components/Navbar-test.js
+++ test/components/Navbar-test.js
@@ -1,28 +1,13 @@
 import React from 'react'
-import expect from 'expect'
 import { createRenderer } from 'react-addons-test-utils'
-import expectJSX from 'expect-jsx'
-expect.extend(expectJSX)
 
 import Navbar from './../../app/components/Navbar'
 
 describe('Navbar', () => {
     it('Navbar works', () => {
-        let renderer = createRenderer()
-        renderer.render(<Navbar />)
-        let actualElement = renderer.getRenderOutput()
-        let expectedElement = ( <Router>
-                <Route path="/" component={App}>
-                <IndexRoute component={Dashboard} />
-                  <Route path="about" component={About} />
-                  <Route path="inbox" component={Inbox}>
-                    <Route path="/messages/:id" component={Message} />
-                    <Redirect from="messages/:id" to="/messages/:id" />
-                  </Route>
-                </Route>
-            </Router>)
-        expect(actualElement).toEqualJSX(expectedElement)
+        let shallowRenderer = createRenderer()
+        let actualResult = shallowRenderer.render(<Navbar/>)
 
-
+        expect(actual.type).toBe('div')
     })
 })
