7a8a6da94140060862f1a6d5b83726d38c788bf5
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-01-25 23:43:02 +0800

    remove useless code

--- app/main.js
+++ app/main.js
@@ -1,22 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Navbar from './components/Navbar.jsx'
-// import Para from './components/test.jsx'
 
-// class Para extends React.Component {
-//     constructor () {
-//         super()
-//         this.eventHandler = this.eventHandler.bind(this)
-//     }
 
-//     eventHandler () {
-//         this.a = 'asdasd'
-//     }
-
-// }
-
-// function main() {
-    ReactDOM.render(<Navbar />, document.getElementById('app'));
-// }
-
-// main()
+ReactDOM.render(<Navbar />, document.getElementById('app'));
