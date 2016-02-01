e1ed183c935b28daaa1789fe2c55b37c81731387
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-02-02 00:10:19 +0800

    will change the way code main.js

--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar'
 
-ReactDOM.render(<Navbar />, document.getElementById('app'));
+ReactDOM.render(<Navbar />, document.getElementById('app'))
