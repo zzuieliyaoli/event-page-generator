f0f08cfebfcf350519a972723d2a3f126e70494b
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-02-04 00:16:00 +0800

    test multer file upload

--- server.js
+++ server.js
@@ -26,44 +26,16 @@ app.get('/', function(request, response) {
 })
 
 app.post(api.uploadImg, function(request, response) {
-    response.json({
-        'message': 'image upload2'
-    })
-    upload(request, response, function(err) {
-            if (err) {
-                throw new Error(err)
 
-            }
-            console.log(request)
-                // Everything went fine
-        })
-        // MongoClient.connect(mongoServerUrl, function(err, db) {
-        //     assert.equal(null, err)
-        //     db.collection('restaurants').insertOne({
-        //         "address": {
-        //             "street": "2 Avenue",
-        //             "zipcode": "10075",
-        //             "building": "1480",
-        //             "coord": [-73.9557413, 40.7720266]
-        //         },
-        //         "borough": "Manhattan",
-        //         "cuisine": "Italian",
-        //         "grades": [{
-        //             "date": new Date("2014-10-01T00:00:00Z"),
-        //             "grade": "A",
-        //             "score": 11
-        //         }, {
-        //             "date": new Date("2014-01-16T00:00:00Z"),
-        //             "grade": "B",
-        //             "score": 17
-        //         }],
-        //         "name": "Vella",
-        //         "restaurant_id": "41704620"
-        //     }, function(err, result) {
-        //         assert.equal(err, null);
-        //         console.log("Inserted a document into the restaurants collection.");
-        //     });
-        // })
+    upload(request, response, function(err) {
+        if (err) {
+            throw new Error(err)
+
+        }
+        console.log(request.file)
+            // Everything went fine
+    })
+        
 
 })
 
