d982f36045c53bb1b64fdf47163bdb15e358a297
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-02-10 22:58:17 +0800

    post and get image from mongodb success

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-'use strict';
+'use strict'
 
 const express = require('express')
 const app = express()
 const Busboy = require('busboy')
 const mongodb = require('mongodb')
+const db = new mongodb.Db('test', new mongodb.Server('127.0.0.1', 27017))
+const Grid = require('gridfs-stream')
 const MongoClient = mongodb.MongoClient
 const assert = require('assert')
+
+// test whether connect mongo server 
 const mongoServerUrl = 'mongodb://localhost:27017/test'
+MongoClient.connect(mongoServerUrl, function(err, db) {
+    assert.equal(null, err)
+    console.log("Connected correctly to server.")
+    db.close()
+})
+
+const gfs
+db.open(function(err, db) {
+    if (err) {
+        throw err
+    }
+    gfs = Grid(db, mongodb)
+})
+
 const api = {
     'uploadImg': '/api/uploadImg'
 }
@@ -25,38 +43,61 @@ app.get('/', function(request, response) {
 
 app.post(api.uploadImg, function(request, response) {
     console.log('post')
-    console.log(request.headers)
-    
-    var busboy = new Busboy({ headers: request.headers })
-    console.log(busboy)
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-        console.log('yo--------------------')
 
-        console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype)
-        file.on('data', function(data) {
-            console.log('File [' + fieldname + '] got ' + data.length + ' bytes')
+    let busboy = new Busboy({
+        headers: request.headers
+    })
+
+    let fileId = new mongodb.ObjectId()
+
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+        console.log('got file', filename, mimetype, encoding)
+        let writeStream = gfs.createWriteStream({
+            _id: fileId,
+            filename: filename,
+            mode: 'w',
+            content_type: mimetype,
         })
-        file.on('end', function() {
-            console.log('File [' + fieldname + '] Finished')
-        })
+
+        file.pipe(writeStream)
     })
 
     busboy.on('finish', function() {
         console.log('Done parsing form!')
-        response.writeHead(303, { Connection: 'close', Location: '/' })
+        console.log('fileId is', fileId)
+        response.writeHead(303, {
+            Connection: 'close',
+            Location: '/'
+        })
         response.end()
     })
 
     request.pipe(busboy)
+})
 
+app.get('/file/:id', function(reqquest, response) {
+    gfs.findOne({
+        _id: reqquest.params.id
+    }, function(err, file) {
+        if (err) return response.status(400).send(err)
+        if (!file) return response.status(404).send('')
+
+        response.set('Content-Type', file.contentType)
+        response.set('Content-Disposition', 'attachment filename="' + file.filename + '"')
+
+        let readstream = gfs.createReadStream({
+            _id: file._id
+        })
+
+        readstream.on("error", function(err) {
+            console.log("Got error while processing stream " + err.message)
+            response.end()
+        })
+
+        readstream.pipe(response)
+    })
 })
 
 app.listen(5000, function() {
     console.log('Express server listening on port 5000')
-})
-
-MongoClient.connect(mongoServerUrl, function(err, db) {
-    assert.equal(null, err)
-    console.log("Connected correctly to server.")
-    db.close()
 })
