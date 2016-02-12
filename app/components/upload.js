46a14692e4c440e04822c6accec51cad9e5a324b
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-02-13 00:03:59 +0800

    add upload

---
+++ app/components/upload.js
@@ -0,0 +1,5 @@
+form(role="form", action="/api/uploadImg", method="post", enctype="multipart/form-data")
+      div(class="form-group")
+        label(for="onixfile") Upload ONIX File        
+        input(type="file", name="onixfile", id="onixfile")
+        input(type="submit")
