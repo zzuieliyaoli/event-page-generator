08a60f25ca884017f32722c8c1728b64f69e572a
zzuieliyaoli <zzuieliyaoli@gmail.com>
2016-02-14 22:45:50 +0800

    add upload component

--- app/components/upload.js
+++ app/components/upload.js
@@ -1,5 +1,9 @@
+import React from 'react'
+
 form(role="form", action="/api/uploadImg", method="post", enctype="multipart/form-data")
       div(class="form-group")
         label(for="onixfile") Upload ONIX File        
         input(type="file", name="onixfile", id="onixfile")
         input(type="submit")
+
+
